c174b622be6b131db7b72e6d6dcf758bb0d505c0
Stefan <stefanandjelic8@gmail.com>
2022-05-12 15:34:24 -0500

    inital migration and ran server successfully

--- todo/settings.py
+++ todo/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4#9p9j6dw+&&nc9o$7!7-ss_6kgv89p)a%)74g*ba)o7i8f&rx'
+from decouple import config
+
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'base.apps.BaseConfig',
 ]
 
 MIDDLEWARE = [
