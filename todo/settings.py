c2a8c01d33f7832a8518757d4a10ae24a927c214
Stefan <stefanandjelic8@gmail.com>
2022-05-13 22:20:46 -0500

    styled task list page

--- todo/settings.py
+++ todo/settings.py
@@ -120,9 +120,9 @@ LOGIN_URL = 'login'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = 'static/'
 
-STATICFILES_DIRS = ('static/',)
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
